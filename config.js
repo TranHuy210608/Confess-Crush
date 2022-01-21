6db33fcb13753d610800a7aca55a56d32f6b1f0c
TranHuy210608 <94668576+TranHuy210608@users.noreply.github.com>
2022-01-21 22:41:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tran Huy",
-    introTitle: 'Đỗ Phương Thảo😍😍',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao chị cứ đi nhầm đường
-    Lạc vào tim em lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "Người tôi yêu:3",
+    introTitle: 'Gửi Vy yêu dấu=)',
+    introDesc: `Sáng nay bước ra khỏi nhà
+Vấp vào viên đá, ngã vào tim em`,
     btnIntro: '^^HiHi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'dưới đất có vạn con người mà sao chị lại yêu em <3',
+    question: 'dưới đất có vạn con người mà sao chế lại yêu mị <3',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'vì em đẹp trai:3',
-    mess: 'em biết mà 🥰. Yêu chị nhiều nhiều 😘😘',
-    messDesc: 'Vậy chị làm người yêu em nha:3.',
+    reply: 'vì mị đẹp trai:3',
+    mess: 'mị biết mà . Yêu chế nhiều nhiều<3 :3',
+    messDesc: 'Vậy em làm người yêu anh nha:3.',
     btnAccept: 'Okiiiii lun <3',
 }
